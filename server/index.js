ae535b983db3560ccf71fb83fb95a4be8d2dedce
preticure <preticure112771@gmail.com>
2024-05-10 12:19:53 +0900

    Initial commit

---
+++ server/index.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const app = express();
+const PORT = 5000;
+require("dotenv").config();
+
+app.use(express.json());
+app.use("/api/v1", require("./src/v1/routes/auth"))
+
+//DB接続
+try {
+  mongoose.connect(process.env.MONGODB_URL);
+  console.log("DBと接続中・・・");
+} catch (error) {
+  console.log("error", error);
+}
+
+
+app.listen(PORT, () => {
+  console.log("ローカルサーバー起動中・・・");
+});
---
+++ server/src/v1/controllers/user.js
@@ -0,0 +1,54 @@
+const CryptoJS = require("crypto-js");
+const JWT = require("jsonwebtoken");
+const User = require("../models/user");
+
+exports.register = async (req, res) => {
+  const password = req.body.password;
+  try {
+    req.body.password = CryptoJS.AES.encrypt(password, process.env.SECRET_KEY);
+    const user = await User.create(req.body);
+    const token = JWT.sign({ id: user._id }, process.env.TOKEN_SECRET_KEY, {
+      expiresIn: "24h",
+    });
+    return res.status(200).json({ user, token });
+  } catch (err) {
+    return res.status(500).json(err);
+  }
+};
+
+exports.login = async (req, res) => {
+  const { username, password } = req.body;
+
+  try {
+    const user = await User.findOne({ username: username });
+    if (!user) {
+      return res.status(401).json({
+        errors: {
+          param: "username",
+          message: "ユーザー名が無効です",
+        },
+      });
+    }
+
+    const decryptedPassword = CryptoJS.AES.decrypt(
+      user.password,
+      process.env.SECRET_KEY
+    ).toString(CryptoJS.enc.Utf8);
+
+    if (decryptedPassword !== password) {
+      return res.status(401).json({
+        errors: {
+          param: "password",
+          message: "パスワードが無効です",
+        },
+      });
+    }
+    const token = JWT.sign({ id: user._id }, process.env.TOKEN_SECRET_KEY, {
+      expiresIn: "24h",
+    });
+
+    return res.status(201).json({ user, token });
+  } catch (err) {
+    return res.status(500).json(err);
+  }
+};
