b0083729ee8ed1f8cf1787cc209262f512e3314f
preticure <preticure112771@gmail.com>
2024-05-13 08:50:00 +0900

    新規登録機能

--- client/src/pages/Register.jsx
+++ client/src/pages/Register.jsx
@@ -2,11 +2,35 @@ import { Box, Button, TextField } from "@mui/material";
 import React from "react";
 import { LoadingButton } from "@mui/lab";
 import { Link } from "react-router-dom";
+import authApi from "../api/authApi";
 
 const Register = () => {
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const data = new FormData(e.target);
+    const username = data.get("username").trim();
+    const password = data.get("password").trim();
+    const confirmPassword = data.get("confirmPassword").trim();
+    console.log(username);
+    console.log(password);
+    console.log(confirmPassword);
+
+    //RegisterAPIを叩く
+    try {
+      const res = await authApi.register({
+        username,
+        password,
+        confirmPassword,
+      });
+      localStorage.setItem("token", res.token);
+      console.log("新規登録に成功しました");
+    } catch (err) {
+      console.log(err);
+    }
+  };
   return (
     <>
-      <Box component="form">
+      <Box component="form" onSubmit={handleSubmit}>
         <TextField
           fullWidth
           id="username"
@@ -29,7 +53,7 @@ const Register = () => {
           id="confirmPassword"
           label="確認用パスワード"
           margin="normal"
-          name="password"
+          name="confirmPassword"
           type="password"
           required
         />
--- server/index.js
+++ server/index.js
@@ -2,10 +2,18 @@ const express = require("express");
 const mongoose = require("mongoose");
 const app = express();
 const PORT = 5000;
+const cors = require("cors");
 require("dotenv").config();
 
+app.use(
+  cors({
+    origin: "http://localhost:3000",
+    credentials: true,
+    optionsSuccessStatus: 200,
+  })
+);
 app.use(express.json());
-app.use("/api/v1", require("./src/v1/routes/auth"))
+app.use("/api/v1", require("./src/v1/routes/index"));
 
 //DB接続
 try {
@@ -14,7 +22,6 @@ try {
 } catch (error) {
   console.log("error", error);
 }
-
 
 app.listen(PORT, () => {
   console.log("ローカルサーバー起動中・・・");
