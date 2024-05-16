45c86ce7a6d6ba04eaf2b89d12c6fdf083541255
preticure <preticure112771@gmail.com>
2024-05-16 22:19:08 +0900

    JWT、token持ってたらホームにリダイレクト

--- client/src/components/layout/AuthLayout.jsx
+++ client/src/components/layout/AuthLayout.jsx
@@ -1,10 +1,24 @@
-import React from "react";
-import { Outlet } from "react-router-dom";
+import React, { useEffect } from "react";
+import { Outlet, useNavigate } from "react-router-dom";
 import { Container } from "@mui/system";
 import { Box } from "@mui/material";
 import notionLogo from "../../assets/images/notion-logo.png";
+import authUtils from "../../utils/authUtils";
 
 const AuthLayout = () => {
+  const navigate = useNavigate();
+  useEffect(() => {
+    //JWTを持っているのか確認する
+    const checkAuth = async () => {
+      //認証チェック
+      const isAuth = await authUtils.isAuthenticated();
+      if (isAuth) {
+        navigate("/");
+      }
+    };
+    checkAuth();
+  }, [navigate]);
+
   return (
     <div>
       <Container component="main" maxWidth="xs">
